6182bb8dace9faa17d9f8e320721194381bce2c7
sharjeelupcastr <sharjeel@upcastr.co>
2026-03-28 14:50:03 +0500

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,9 @@ import { createMDX } from "fumadocs-mdx/next";
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
+  typedRoutes: true
 };
 
 const withMDX = createMDX();
 
 export default withMDX(config);
-
