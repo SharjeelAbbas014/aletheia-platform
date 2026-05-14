4b178ebe69f42b019c6743b9cad847ac6239a8f3
sharjeelupcastr <sharjeel@upcastr.co>
2026-05-14 13:39:53 +0500

    Fix sitemap

--- src/routes/sitemap.xml/index.ts
+++ src/routes/sitemap.xml/index.ts
@@ -1,8 +1,6 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = (event) => {
-  event.headers.set("Content-Type", "application/xml; charset=utf-8");
-
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -23,5 +21,10 @@ export const onGet: RequestHandler = (event) => {
 </urlset>
 `;
 
-  event.send(200, body);
+  event.send(
+    new Response(body, {
+      status: 200,
+      headers: { "Content-Type": "application/xml; charset=utf-8" },
+    })
+  );
 };
